600a0a47093c867d2f4a1d01da1e1131607e4d4f
Robert Laws <roblaws@me.com>
2019-04-21 20:47:35 +0300

    update datetime challenge

--- date-and-times/challenge.rb
+++ date-and-times/challenge.rb
@@ -1,3 +1,5 @@
+require 'date'
+
 # Challenge - Birthdate Analysis
 
 # Ask user for the year month and date of their birth
@@ -5,6 +7,8 @@
 # Tell date of the year as number
 # Tell the week of the year
 # Whether it was a leap year
+
+days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
 
 puts "|---------------------|"
 puts "|  Birthday Analysis  |"
@@ -19,11 +23,13 @@ month = gets.chomp
 puts "What date of the month were you born"
 day = gets.chomp
 
-bday = Time.new(year, month, day)
+bday = Date.new(year.to_i, month.to_i, day.to_i)
 # bday = Time.new(1975, 12, 22)
 puts "-----------------------"
 puts "You were born on a #{bday.strftime('%A')}."
+puts "You were born on a #{days[bday.wday]}."
 puts "It was the #{bday.yday / 7} week of the year"
+puts "It was the #{bday.cweek} week of the year"
 puts "It was the #{bday.yday} day of the year"
 puts "You were #{bday.year % 4 == 0 ? '' : 'not ' }born in a leap year."
 puts "-----------------------"
